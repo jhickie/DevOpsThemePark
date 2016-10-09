74ca401df6cefa7e315452807d399b08d52430d7
Steve Ganly <steve.ganly@gmail.com>
2016-10-09 20:56:18 +0100

    Added some more notes on how the bootstrap is supposed to work.

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 # Script to check and/or install software packages to build the attractions.
+#
+# The idea is to be able to run something like this from a new machine
+#   curl https://github.com/ganly/DevOpsThemePark/blob/master/lib/bootstrap.sh | sh
+# or if this repo has been cloned it can be run locally.
+#
+#
+# We probably want to have the default action to check basic setup,
+# and command line options to check or install things for particular features, e.g.
+#   bootstrap.sh --basicChefExamples
